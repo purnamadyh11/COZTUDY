b034b568f82abcf39bfbc70500f6c425b9f5f7a7
purnamadyh11 <purnamadyh@gmail.com>
2024-06-20 14:33:15 +0700

    Update frontend.php

--- routes/frontend.php
+++ routes/frontend.php
@@ -1,12 +1,13 @@
 <?php
 
+use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Frontend\CartController;
 use App\Http\Controllers\Frontend\FrontendController;
 use App\Http\Controllers\Frontend\TransactionController;
 
 Route::get('/', [FrontendController::class, 'index'])->name('index');
 Route::get('/login', [FrontendController::class, 'login']);
-Route::get('/register', [FrontendController::class, 'register'])->name('actionregister');
+Route::get('/register', [FrontendController::class, 'register']);
 Route::get('/product', [FrontendController::class, 'product']);
 Route::get('/detail/{id}', [FrontendController::class, 'detail']);
 
@@ -18,3 +19,5 @@ Route::get('/cart/delete/{id}', [CartController::class, 'delete']);
 // transaction
 Route::post('/transaction/store', [TransactionController::class, 'store']);
 Route::get('/transaction/cancel/{id}', [TransactionController::class, 'cancel']);
+
+Route::post('register/action', [RegisterController::class, 'actionregister'])->name('actionregister');
